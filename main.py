9c7b8a22f6a70113979e103cd8bb52608ff7c062
rahul13085 <168804452+rahul13085@users.noreply.github.com>
2024-05-18 14:45:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from telegram import Update, ForceReply, File
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 from config import BOT_TOKEN, API_HASH
 from asyncio import Queue
+from telegram.ext import Updater, Dispatcher
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 update_queue = Queue()
+updater = Updater(BOT_TOKEN)
+dispatcher = Dispatcher(updater)
 updater = Updater(BOT_TOKEN, update_queue=update_queue)
 dispatcher = updater.dispatcher
 
