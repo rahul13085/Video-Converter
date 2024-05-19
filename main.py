529cb0229217fa51d22109a7e62a8cd465f55b9c
rahul13085 <168804452+rahul13085@users.noreply.github.com>
2024-05-19 16:38:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import logging
 import asyncio
 import nest_asyncio
-import tracemalloc
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
